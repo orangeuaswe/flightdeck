0bc78d5838bd2b86a0d1073132f35ab6db81b158
Deveram <anirudhdeveram@gmail.com>
2026-08-06 22:23:29 -0400

    Add files via upload
    initial commit, just setting things up

---
+++ fdeck/backend/src/test/java/com/fdeck/flightdeck/FlightdeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdeck.flightdeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightdeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
